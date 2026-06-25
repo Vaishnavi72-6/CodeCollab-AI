32b936de9c43b332595c5504e9277890d64a0224
Vaishnavi7206 <polimeraravi2006@gmail.com>
2026-06-25 07:27:03 +0530

    Implemented JWT authentication and protected routes

--- backend/app/routes/auth.py
+++ backend/app/routes/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-
+from fastapi.security import OAuth2PasswordRequestForm
 from app.database import get_db
 from app.models.user import User
 from app.schemas.user_schema import UserRegister
@@ -8,6 +8,7 @@ from app.auth.hashing import hash_password
 from app.auth.hashing import verify_password
 from app.auth.jwt_handler import create_access_token
 from app.schemas.user_schema import UserLogin
+from app.dependencies.auth import get_current_user
 router = APIRouter(
     prefix="/auth",
     tags=["Authentication"]
@@ -44,12 +45,12 @@ def register(
     }
 @router.post("/login")
 def login(
-    user: UserLogin,
+    form_data: OAuth2PasswordRequestForm = Depends(),
     db: Session = Depends(get_db)
 ):
 
     db_user = db.query(User).filter(
-        User.email == user.email
+        User.email == form_data.username
     ).first()
 
     if not db_user:
@@ -58,7 +59,7 @@ def login(
         }
 
     if not verify_password(
-        user.password,
+        form_data.password,
         db_user.password_hash
     ):
         return {
@@ -74,4 +75,14 @@ def login(
     return {
         "access_token": token,
         "token_type": "bearer"
+    }
+@router.get("/me")
+def current_user(
+    user: User = Depends(get_current_user)
+):
+
+    return {
+        "id": user.id,
+        "name": user.name,
+        "email": user.email
     }
